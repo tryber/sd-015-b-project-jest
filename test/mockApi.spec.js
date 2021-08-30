78ce78edf2ae776014121c9680fb02dfe0187a1e
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-08-30 13:48:03 -0300

    terminado 3 requisito

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -25,7 +25,22 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 describe('2 - Verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
 
-  test('verifica se o usuário é o tunico', async () => (
+  api.fetchURL = jest.fn().mockResolvedValue(
+    {
+      gender: 'male',
+      name: { first: 'Antônio', last: 'Britto' },
+      location: {
+        country: 'Brazil',
+      },
+      email: 'tunico@bol.com.br',
+      login: {
+        username: 'tunicao123',
+        password: '1234567890',
+      },
+    },
+  );
+
+  it('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
