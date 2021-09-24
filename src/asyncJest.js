9f328e0f57d31b4d57a1b0e90dd7d6ecb3642c1c
Rafael Toledo <rafael.toledo@engenharia.ufjf.br>
2021-09-24 14:49:21 -0300

    test

--- src/asyncJest.js
+++ src/asyncJest.js
@@ -3,7 +3,7 @@ const answerPhone = (answer) =>
   new Promise(function (resolve, reject) {
     setTimeout(
       answer ? resolve('Oi!') : reject(new Error('Infelizmente não podemos atender...')), 3000,
-    ); 
+    );
   });
 
 module.exports = answerPhone;
