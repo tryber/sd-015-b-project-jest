649541d115e8bd055a0bd48a1b1b519fd288115f
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-09-24 15:08:46 -0300

    requisito 1 com lint ok

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -14,7 +13,7 @@ describe('1 - O retorno do telefonema', () => {
   test('atende', async () => {
     // na primeira linha precisamos passar a função do teste para assincrona, já que ela vai testar uma promise assim utilizamos async.
     await answerPhone(true).then((resolve) => {
-      // utilizamos o await para que espere essa função terminar, como a answerPhone() gera uma promise, e esperamos que seja resolvida pois usamos true como parametro, utilizamos  o  .then(e usando o valor gerado na promise como parametro), 
+      // utilizamos o await para que espere essa função terminar, como a answerPhone() gera uma promise, e esperamos que seja resolvida pois usamos true como parametro, utilizamos  o  .then(e usando o valor gerado na promise como parametro),
       expect(resolve).toEqual('Oi!');
       // /jogamos em um expect, que diz que se esse resolve capturado pelo then, tem de ser estritamento igual á 'Oi!'
     });
