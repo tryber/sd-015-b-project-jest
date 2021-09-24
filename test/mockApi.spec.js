56b6c7cb94aba62bc5a0f0ec03c4f3b1dacc5f1d
Ge <gmarcoscardoso@gmail.com>
2021-09-24 17:07:17 -0300

    Requisito 03: :heavy_check_mark:

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,8 +24,16 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('2 - Verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const userInfo = {
+    gender: 'male',
+    name: { last: 'Britto', first: 'Antônio' },
+    location: { country: 'Brazil' },
+    email: 'tunico@bol.com.br',
+    login: { username: 'tunicao123', password: '1234567890' },
+  };
+  api.fetchURL = jest.fn().mockResolvedValue(() => Promise.resolve(userInfo));
 
-  test('verifica se o usuário é o tunico', async () => (
+  test('verifica se o usuário é o tunico', () => {
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
@@ -34,6 +42,6 @@ describe('2 - Verifica o usuário', () => {
       expect(user.email).toEqual('tunico@bol.com.br');
       expect(user.login.username).toEqual('tunicao123');
       expect(user.login.password).toEqual('1234567890');
-    })
-  ));
+    });
+  });
 });
