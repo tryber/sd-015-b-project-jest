d8d9e309b32c9895640143a0efb28f76b6fda6bd
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-09-24 17:04:20 -0300

    Requisito 2, simulando o retorno de uma api

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -21,10 +21,15 @@ Dica: Utilizem os métodos jest.fn() ou jest.spyOn().
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
-
+api.fetchURL = jest.fn().mockImplementation(async () => ({
+  gender: 'male',
+  name: { first: 'Antônio', last: 'Britto' },
+  location: { country: 'Brazil' },
+  email: 'tunico@bol.com.br',
+  login: { username: 'tunicao123', password: '1234567890' },
+}));
 describe('2 - Verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
-
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
