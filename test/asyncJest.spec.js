b0d487033bab789835d400695285d52342440d75
ArthurMoreira <arthurmcs96@gmail.com>
2021-09-24 16:05:06 -0300

    Testando o requisito 1

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -11,9 +11,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('1 - O retorno do telefonema', () => {
-  test('atende', () => answerPhone(true).then((response) => expect(response).toEqual('Oi!')));
+  test('atende', () => answerPhone(true).then((response) => expect(response).toBe('Oi!')));
   test('ocupado', () => {
     answerPhone(false).catch((response) =>
-      expect(response).toEqual('Infelizmente não podemos atender...'));
+      expect(response).toBe('Infelizmente não podemos atender...'));
   });
 });
