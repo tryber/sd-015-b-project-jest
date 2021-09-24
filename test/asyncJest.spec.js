c2170d7161e8415a4e259f4f01935b9752bbae2e
Virginia <vihvasc@gmail.com>
2021-09-24 13:41:22 -0300

    Iniciando o projeto Jest

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -17,6 +17,6 @@ describe('1 - O retorno do telefonema', () => {
   });
   test('ocupado', () => {
     assert.fail();
-    // Insira seu teste assíncrono aqui
+    // Insira seu teste assíncrono aquii
   });
 });
