1eed6b73aa1809f84e02088741f55cc00390188b
Rodolpho Coller <rodolpho.coller12@gmail.com>
2021-09-24 16:59:36 -0300

    implementa testes ao arquivo mockApi.spec.js

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -24,8 +24,22 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('2 - Verifica o usuário', () => {
   // Crie sua mock da função fetchURL() aqui
+  const dataUser = {
+    gender: 'male',
+    name: {
+      first: 'Antônio',
+      last: 'Britto',
+    },
+    location: { country: 'Brazil' },
+    email: 'tunico@bol.com.br',
+    login: {
+      username: 'tunicao123',
+      password: '1234567890',
+    },
+  };
+  api.fetchURL = jest.fn().mockResolvedValue(dataUser);
 
-  test('verifica se o usuário é o tunico', async () => (
+  it('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
       expect(user.gender).toEqual('male');
       expect(user.name.first).toEqual('Antônio');
