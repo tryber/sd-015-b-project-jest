a16f0039c92ca0e760c96d6da393fa66b75e0ca4
Danielen Cestari Nunes <danielen.cestari@gmail.com>
2021-09-24 16:53:56 -0300

    corrige erros lint

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,13 +1,11 @@
-//const assert = require('assert');
+// const assert = require('assert');
 //const { resourceLimits } = require('worker_threads');
 const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
-
 Complete o código abaixo para testar as situações em que
 a função recebe como parâmetro true e false, respectivamente.
-
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
@@ -16,12 +14,10 @@ describe('1 - O retorno do telefonema', () => {
     answerPhone(true).then((resolve) => {
       expect(resolve).toEqual('Oi!');
     });
-    
-   // se a função executada na variavel for verdadeira (valor boleano) a resposta do resultado do teste deve ser igual ao resolve da promise. 
+    // se a função executada na variavel for verdadeira (valor boleano) a resposta do resultado do teste deve ser igual ao resolve da promise. 
   });
   test('ocupado', () => {
-/* Certifique-se de retornar a promise - se você omitir esta instrução retorna, seu teste será concluído antes que a promessa retornada por fetchData resolva e then() tenha a chance de executar a callback.
- */   
+    /* Certifique-se de retornar a promise - se você omitir esta instrução retorna, seu teste será concluído antes que a promessa retornada por fetchData resolva e then() tenha a chance de executar a callback.*/
     answerPhone(false).catch((error) => {
       expect(error.message).toMatch('Infelizmente não podemos atender...');
     });
