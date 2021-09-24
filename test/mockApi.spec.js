85c4f547559b72ec5932e82e3373018564fb0f74
David Souza <dabcs@poli.ufrj.br>
2021-09-24 12:04:31 -0300

    resolução do requisito 3

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -1,5 +1,58 @@
 const api = require('../src/mockApi');
 
+const apiRequest = {
+  gender: 'male',
+  name: {
+    title: 'mr',
+    first: 'Antônio',
+    last: 'Britto',
+  },
+  location: {
+    country: 'Brazil',
+    street: 'Rua: Rio de Janeiro, 28',
+    city: 'Belo Horizonte',
+    state: 'Minas Gerais',
+    postcode: '37734-000',
+    coordinates: {
+      latitude: '-20.9267',
+      longitude: '-7.9310',
+    },
+    timezone: {
+      offset: '-3:30',
+      description: 'Newfoundland',
+    },
+  },
+  email: 'tunico@bol.com.br',
+  login: {
+    uuid: '155e77ee-ba6d-486f-95ce-0e0c0fb4b919',
+    username: 'tunicao123',
+    password: '1234567890',
+    salt: 'TQA1Gz7x',
+    md5: 'dc523cb313b63dfe5be2140b0c05b3bc',
+    sha1: '7a4aa07d1bedcc6bcf4b7f8856643492c191540d',
+    sha256: '74364e96174afa7d17ee52dd2c9c7a4651fe1254f471a78bda0190135dcd3480',
+  },
+  dob: {
+    date: '1993-07-20T09:44:18.674Z',
+    age: 26,
+  },
+  registered: {
+    date: '2002-05-21T10:59:49.966Z',
+    age: 17,
+  },
+  phone: '011-962-7516',
+  cell: '081-454-0666',
+  id: {
+    name: 'PPS',
+    value: '0390511T',
+  },
+  picture: {
+    large: 'https://randomuser.me/api/portraits/men/75.jpg',
+    medium: 'https://randomuser.me/api/portraits/med/men/75.jpg',
+    thumbnail: 'https://randomuser.me/api/portraits/thumb/men/75.jpg',
+  },
+  nat: 'IE',
+};
 /*
 A função fetchURL retorna um JSON com informações de um usuário aleatório buscadas da API 'randomuser.me'.
 No entanto, nos testes abaixo, queremos que todas as vezes que chamarmos a API a resposta contenha as informações do nosso adminis..Cof! Cof!.. programador favorito, Tunicão.
@@ -23,7 +76,9 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('2 - Verifica o usuário', () => {
-  // Crie sua mock da função fetchURL() aqui
+  api.fetchURL = jest
+    .fn()
+    .mockImplementation(() => new Promise((resolve) => resolve(apiRequest)));
 
   test('verifica se o usuário é o tunico', async () => (
     api.fetchURL().then((user) => {
