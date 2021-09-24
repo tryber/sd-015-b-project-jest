eda466580f8d0da17a4183692dc7dff51f270622
Gabrielle-Murat <belle.murat@gmail.com>
2021-09-24 13:25:22 -0300

    Conserta erros de lint

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,6 +1,6 @@
-const assert = require('assert');
+// const assert = require('assert');
 const answerPhone = require('../src/asyncJest');
-// const answerPhone = require('../src/asyncJest');
+
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -12,12 +12,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('1 - O retorno do telefonema', () => {
-
-  test('atende', () => {
-    return expect(answerPhone(true))
+  test('atende', () =>
+    expect(answerPhone(true))
       .resolves
-      .toEqual('Oi!');
-  });
+      .toEqual('Oi!'));
 
   // test('atende', () => (
   //   answerPhone(true)
