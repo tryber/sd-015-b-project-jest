f537883dade317d981d16ac826846b441a360300
Arthur Bernardo Messias <turmessias@gmail.com>
2021-09-24 16:18:51 -0300

    Cumpre Requisito 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -19,22 +19,25 @@ PS: Os codinomes dos aventureiros são reais! Tentem descobrir quem é quem!
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
-describe('4 - Quem sobreviveu?', () => {
+describe('quem sobreviveu?', () => {
   // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.randomAttack();
+  });
 
-  test('depois da primeira aventura', () => {
+  it('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
   });
-  test('depois da segunda aventura', () => {
+  it('depois da segunda aventura', () => {
     expect(adventure.specialists.length).toBe(4);
   });
-  test('depois da terceira aventura', () => {
+  it('depois da terceira aventura', () => {
     expect(adventure.specialists.length).toBe(3);
   });
-  test('depois da quarta aventura', () => {
+  it('depois da quarta aventura', () => {
     expect(adventure.specialists.length).toBe(2);
   });
-  test('depois da quinta aventura', () => {
+  it('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
   });
 });
