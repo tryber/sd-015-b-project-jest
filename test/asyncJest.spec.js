2c8dda85f9f9ac99deba5d98288f4b3bcbfe8674
Danielen Cestari Nunes <danielen.cestari@gmail.com>
2021-09-24 16:16:53 -0300

    requisito 01 testado - ajuda do instrutor Fernando

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,5 @@
 const assert = require('assert');
+const { resourceLimits } = require('worker_threads');
 // const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
@@ -9,14 +10,21 @@ a função recebe como parâmetro true e false, respectivamente.
 
 ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
+const answerPhone = require('../src/asyncJest')
 
 describe('1 - O retorno do telefonema', () => {
   test('atende', () => {
+    answerPhone(true).then((resolve) => {
+      expect(resolve).toEqual('Oi!');
+    })
     
-    // Insira seu teste assíncrono aqui
+   // se a função executada na variavel for verdadeira (valor boleano) a resposta do resultado do teste deve ser igual ao resolve da promise. 
   });
   test('ocupado', () => {
-    
-    // Insira seu teste assíncrono aqui
+/* Certifique-se de retornar a promise - se você omitir esta instrução retorna, seu teste será concluído antes que a promessa retornada por fetchData resolva e then() tenha a chance de executar a callback.
+ */   
+   answerPhone(false).catch((error) => {
+      expect(error.message).toMatch('Infelizmente não podemos atender...');
+    })
   });
 });
