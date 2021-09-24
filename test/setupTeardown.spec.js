e3dbfb43bab85fdc784f4500919a00c4b328316c
aaguero96 <andre.luiz_1996@hotmail.com>
2021-09-24 20:11:05 -0300

    Fix lint part 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -29,22 +29,33 @@ describe('4 - Quem sobreviveu?', () => {
   beforeEach(() => {
     adventure.randomAttack();
   });
+
   adventure.randomAttack = jest.spyOn(adventure, 'randomAttack');
-  test('depois da primeira aventura', () => {
-    expect(adventure.specialists.length).toBe(5);
+
+  [...Array(5).keys()].forEach((element) => {
+    const numberOfTest = element + 1;
+    const testText = `depois da ${numberOfTest} aventura`;
+    const survivals = 5 - element;
+    test(testText, () => {
+      expect(adventure.specialists.length).toBe(survivals);
+    });
   });
-  test('depois da segunda aventura', () => {
-    expect(adventure.specialists.length).toBe(4);
-  });
-  test('depois da terceira aventura', () => {
-    expect(adventure.specialists.length).toBe(3);
-  });
-  test('depois da quarta aventura', () => {
-    expect(adventure.specialists.length).toBe(2);
-  });
-  test('depois da quinta aventura', () => {
-    expect(adventure.specialists.length).toBe(1);
-  });
+
   const lastSurviver = adventure.specialists[0].nome;
   console.log(`The last surviver is ${lastSurviver}`);
+  // test('depois da primeira aventura', () => {
+  //   expect(adventure.specialists.length).toBe(5);
+  // });
+  // test('depois da segunda aventura', () => {
+  //   expect(adventure.specialists.length).toBe(4);
+  // });
+  // test('depois da terceira aventura', () => {
+  //   expect(adventure.specialists.length).toBe(3);
+  // });
+  // test('depois da quarta aventura', () => {
+  //   expect(adventure.specialists.length).toBe(2);
+  // });
+  // test('depois da quinta aventura', () => {
+  //   expect(adventure.specialists.length).toBe(1);
+  // });
 });
