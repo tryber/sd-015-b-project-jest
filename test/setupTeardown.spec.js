ed9d9c89b240d324a2ae3da2a1dd5fd551bfcae0
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-09-24 20:11:24 -0300

    Adiciona requisito 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -21,6 +21,12 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('4 - Quem sobreviveu?', () => {
   // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.randomAttack();
+  });
+  afterEach(() => {
+    console.log(adventure.specialists.length);
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
