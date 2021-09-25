4032855bb84e4ca836f6411b41c6e0afa6373658
Carlos Lima <carlosedu151@gmail.com>
2021-09-25 20:32:39 -0300

    Tentando outra abordagem, não identifiquei o erro na anterior

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,6 +1,4 @@
 const mockFunctions = require('../src/mockFunctions');
-
-jest.mock('../src/mockFunctions');
 /*
 Criamos uma série de funções com eficiência duvidosa.
 Elas estão no arquivo 'src/mockFunctions.js'.
@@ -16,18 +14,18 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('3 - Verifica as funções e os mocks', () => {
-  mockFunctions.multiply.mockImplementation((a, b) => a * b);
-  mockFunctions.power.mockImplementation((a, b) => a ** b);
-  mockFunctions.factorial.mockImplementation((n) => { // O código dessa função foi retirado do site https://serprogramador.com.br/artigos/topico/javascript/Como-calcular-a-operacao-matematica-de-fatorial-com-JavaScript
+  mockFunctions.multiply = jest().mockImplementation((a, b) => a * b);
+  mockFunctions.power = jest().mockImplementation((a, b) => a ** b);
+  mockFunctions.factorial = jest().mockImplementation((n) => { // O código dessa função foi retirado do site https://serprogramador.com.br/artigos/topico/javascript/Como-calcular-a-operacao-matematica-de-fatorial-com-JavaScript
     if (n === 0 || n === 1) return 1;
     for (let index = n - 1; index >= 1; index -= 1) {
       n *= index;
     }
     return n;
   });
-  mockFunctions.add.mockImplementation((a, b) => a + b);
-  mockFunctions.subtract.mockImplementation((a, b) => a - b);
-  mockFunctions.divide.mockImplementation((a, b) => a / b);
+  mockFunctions.add = jest().mockImplementation((a, b) => a + b);
+  mockFunctions.subtract = jest().mockImplementation((a, b) => a - b);
+  mockFunctions.divide = jest().mockImplementation((a, b) => a / b);
 
   test('testa função add', () => {
     expect(mockFunctions.add(1, 2)).toEqual(3);
