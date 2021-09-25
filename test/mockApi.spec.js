b89f3e4b4d884c1dd8e8d58a952839d40112895f
Daniel Dias <danieldias.b99@hotmail.com>
2021-09-25 16:33:03 -0300

    Implementa estrutura assincrona para o Mock.

--- test/mockApi.spec.js
+++ test/mockApi.spec.js
@@ -40,15 +40,14 @@ describe('2 - Verifica o usuário', async() => {
 
   api.fetchURL = jest.fn().mockResolvedValue(mockObj);
 
-  test('verifica se o usuário é o tunico', async () => (
-    api.fetchURL().then((user) => {
-      expect(user.gender).toEqual('male');
-      expect(user.name.first).toEqual('Antônio');
-      expect(user.name.last).toEqual('Britto');
-      expect(user.location.country).toEqual('Brazil');
-      expect(user.email).toEqual('tunico@bol.com.br');
-      expect(user.login.username).toEqual('tunicao123');
-      expect(user.login.password).toEqual('1234567890');
-    })
-  ));
+  test('verifica se o usuário é o tunico', async () => {
+    const user = await api.fetchURL();
+    expect(user.gender).toEqual('male');
+    expect(user.name.first).toEqual('Antônio');
+    expect(user.name.last).toEqual('Britto');
+    expect(user.location.country).toEqual('Brazil');
+    expect(user.email).toEqual('tunico@bol.com.br');
+    expect(user.login.username).toEqual('tunicao123');
+    expect(user.login.password).toEqual('1234567890');
+  });
 });
