290235f3f8ad63ebf3add059c044c0e41b2a93e6
Gabriel Pereira Benedicto <gabrielpbenedicto@gmail.com>
2021-09-25 04:28:55 -0300

    Requisito 4

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -20,7 +20,10 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 */
 
 describe('4 - Quem sobreviveu?', () => {
-  // Adicione seu código aqui
+  beforeEach(() => {
+    adventure.randomAttack();
+    console.table(adventure.specialists);
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
@@ -36,5 +39,7 @@ describe('4 - Quem sobreviveu?', () => {
   });
   test('depois da quinta aventura', () => {
     expect(adventure.specialists.length).toBe(1);
+    console.log(`The last Survived is ${adventure.specialists[0].nome}
+    the ${adventure.specialists[0].classe}`);
   });
 });
