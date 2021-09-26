33cc14c258d57f58896b6d7f22a4199ce8cbdf63
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-09-26 08:42:04 -0300

    [Antonio-C-N-Silva-Jr] Iniciando o Projeto Jest

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -12,11 +12,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('1 - O retorno do telefonema', () => {
   test('atende', () => {
-    assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
   });
   test('ocupado', () => {
-    assert.fail();
+    // assert.fail();
     // Insira seu teste assíncrono aqui
   });
 });
