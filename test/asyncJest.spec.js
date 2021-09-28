bf195385901928a0dfa752e9eff64646f439252a
iuryroque <iury.roque@hotmail.com>
2021-09-27 21:41:14 -0300

    Revert "Requisito 1 finalizado"
    This reverts commit e354cafce33e9f7d361674f8beccf0bbf34cb934.

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,4 +1,5 @@
-const answerPhone = require('../src/asyncJest');
+const assert = require('assert');
+// const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -11,13 +12,11 @@ ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
 
 describe('1 - O retorno do telefonema', () => {
   test('atende', () => {
-    answerPhone(true).then((result) => {
-      expect(result).toEqual('Oi!');
-    });
+    assert.fail();
+    // Insira seu teste assíncrono aqui
   });
   test('ocupado', () => {
-    answerPhone(false).catch((error) => {
-      expect(error.message).toEqual('Infelizmente não podemos atender...');
-    });
+    assert.fail();
+    // Insira seu teste assíncrono aqui
   });
 });
