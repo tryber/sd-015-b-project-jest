727a0c854d78a4e66749af1995d1b4525a3df414
Mariah M. Rodrigues <mariah.manetti@gmail.com>
2021-09-28 01:28:47 -0300

    Requisito - Correçao linha 19 (-resolves +rejects)

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,5 +1,4 @@
 const answerPhone = require('../src/asyncJest');
-// const answerPhone = require('../src/asyncJest');
 /*
 A função answerPhone recebe um parâmetro boleano.
 Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
@@ -17,6 +16,6 @@ describe('1 - O retorno do telefonema', () => {
   });
   test('ocupado', () => {
     // Insira seu teste assíncrono aqui
-    expect(answerPhone(false)).resolves.toEqual(new Error('Infelizmente não podemos atender...'));
+    expect(answerPhone(false)).rejects.toEqual(new Error('Infelizmente não podemos atender...'));
   });
 });
