1c204d9ed7aa283447c33929d2ad300db23ef739
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-09-28 16:06:46 -0300

    Finaliza Requisito 2

--- test/mockFunctions.spec.js
+++ test/mockFunctions.spec.js
@@ -1,9 +1,10 @@
+const rFact = require('./factorial');
 const mockFunctions = require('../src/mockFunctions');
-
 /*
 Criamos uma série de funções com eficiência duvidosa.
 Elas estão no arquivo 'src/mockFunctions.js'.
-Crie mock functions para cada uma das operações de modo que os cálculos sejam feitos corretamente,
+Crie mock functions para cada uma das operações de modo que os cálculos sejam
+feitos corretamente,
 não como estão sendo feitos no arquivo original.
 A idéia é que os novos testes sobrescrevam os testes
 importados apenas na suite de testes abaixo.
@@ -18,6 +19,14 @@ describe('3 - Verifica as funções e os mocks', () => {
   // Crie suas mock functions aqui
 
   test('testa função add', () => {
+    mockFunctions.add = jest
+      .fn()
+      .mockReturnValueOnce(1 + 2)
+      .mockReturnValueOnce(8 + 37)
+      .mockReturnValueOnce(-11 + 25)
+      .mockReturnValueOnce(13 - 188)
+      .mockReturnValueOnce(7 + 26);
+
     expect(mockFunctions.add(1, 2)).toEqual(3);
     expect(mockFunctions.add(8, 37)).toEqual(45);
     expect(mockFunctions.add(-11, 25)).toEqual(14);
@@ -25,6 +34,14 @@ describe('3 - Verifica as funções e os mocks', () => {
     expect(mockFunctions.add(7, 26)).toEqual(33);
   });
   test('testa função subtract', () => {
+    mockFunctions.subtract = jest
+      .fn()
+      .mockReturnValueOnce(899 - 35)
+      .mockReturnValueOnce(-17 - 333)
+      .mockReturnValueOnce(45 - 97)
+      .mockReturnValueOnce(23 + 108)
+      .mockReturnValueOnce(-133 + 29);
+
     expect(mockFunctions.subtract(899, 35)).toEqual(864);
     expect(mockFunctions.subtract(-17, 333)).toEqual(-350);
     expect(mockFunctions.subtract(45, 97)).toEqual(-52);
@@ -32,6 +49,14 @@ describe('3 - Verifica as funções e os mocks', () => {
     expect(mockFunctions.subtract(-133, -29)).toEqual(-104);
   });
   test('testa função multiply', () => {
+    mockFunctions.multiply = jest
+      .fn()
+      .mockReturnValueOnce(1 * 2)
+      .mockReturnValueOnce(0 * 5)
+      .mockReturnValueOnce(-4 * 9)
+      .mockReturnValueOnce(-12 * (-7))
+      .mockReturnValueOnce(19 * 23);
+
     expect(mockFunctions.multiply(1, 2)).toEqual(2);
     expect(mockFunctions.multiply(0, 5)).toEqual(0);
     expect(mockFunctions.multiply(-4, 9)).toEqual(-36);
@@ -39,6 +64,14 @@ describe('3 - Verifica as funções e os mocks', () => {
     expect(mockFunctions.multiply(19, 23)).toEqual(437);
   });
   test('testa função divide', () => {
+    mockFunctions.divide = jest
+      .fn()
+      .mockReturnValueOnce(169 / 13)
+      .mockReturnValueOnce(-1900 / 5)
+      .mockReturnValueOnce(42 / 7)
+      .mockReturnValueOnce(729 / 243)
+      .mockReturnValueOnce(1331 / 11);
+
     expect(mockFunctions.divide(169, 13)).toEqual(13);
     expect(mockFunctions.divide(-1900, 5)).toEqual(-380);
     expect(mockFunctions.divide(42, 7)).toEqual(6);
@@ -46,6 +79,14 @@ describe('3 - Verifica as funções e os mocks', () => {
     expect(mockFunctions.divide(1331, 11)).toEqual(121);
   });
   test('testa função power', () => {
+    mockFunctions.power = jest
+      .fn()
+      .mockReturnValueOnce(10 ** 2)
+      .mockReturnValueOnce(2 ** 10)
+      .mockReturnValueOnce(5 ** 5)
+      .mockReturnValueOnce(1 ** 10)
+      .mockReturnValueOnce(0 ** 0);
+
     expect(mockFunctions.power(10, 2)).toEqual(100);
     expect(mockFunctions.power(2, 10)).toEqual(1024);
     expect(mockFunctions.power(5, 5)).toEqual(3125);
@@ -53,6 +94,14 @@ describe('3 - Verifica as funções e os mocks', () => {
     expect(mockFunctions.power(0, 0)).toEqual(1);
   });
   test('testa função factorial', () => {
+    mockFunctions.factorial = jest
+      .fn()
+      .mockReturnValueOnce(rFact(5))
+      .mockReturnValueOnce(rFact(10))
+      .mockReturnValueOnce(rFact(3))
+      .mockReturnValueOnce(rFact(8))
+      .mockReturnValueOnce(rFact(2));
+
     expect(mockFunctions.factorial(5)).toEqual(120);
     expect(mockFunctions.factorial(10)).toEqual(3628800);
     expect(mockFunctions.factorial(3)).toEqual(6);
