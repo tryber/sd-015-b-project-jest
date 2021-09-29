a089be9d460a254e7bd203f23959c97e9aa67849
fariasao <enzofarias2010@gmail.com>
2021-09-28 21:04:53 -0300

    teste evaluator

--- test/asyncJest.spec.js
+++ test/asyncJest.spec.js
@@ -1,24 +1,24 @@
-// const assert = require('assert');
-const answerPhone = require('../src/asyncJest');
-/*
-A função answerPhone recebe um parâmetro boleano.
-Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
+// // const assert = require('assert');
+// const answerPhone = require('../src/asyncJest');
+// /*
+// A função answerPhone recebe um parâmetro boleano.
+// Dependendo do parâmetro o retorno da função varia, veja a função no arquivo 'src/asyncJest.js'
 
-Complete o código abaixo para testar as situações em que
-a função recebe como parâmetro true e false, respectivamente.
+// Complete o código abaixo para testar as situações em que
+// a função recebe como parâmetro true e false, respectivamente.
 
-ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
-*/
+// ATENÇÃO!!! Edite apenas este arquivo. Não altere os arquivos da pasta 'src'.
+// */
 
-describe('1 - O retorno do telefonema', () => {
-  test('atende', () => {
-    answerPhone(true).then((answer) => {
-      expect(answer).toBe('Oi!');
-    });
-  });
-  test('ocupado', () => {
-    answerPhone(false).catch((error) => {
-      expect(error.message).toThrow('Infelizmente não podemos atender...');
-    });
-  });
-});
+// describe('1 - O retorno do telefonema', () => {
+//   test('atende', () => {
+//     answerPhone(true).then((answer) => {
+//       expect(answer).toBe('Oi!');
+//     });
+//   });
+//   test('ocupado', () => {
+//     answerPhone(false).catch((error) => {
+//       expect(error.message).toThrow('Infelizmente não podemos atender...');
+//     });
+//   });
+// });
