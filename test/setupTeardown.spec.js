e02765a9f3469bf88a5d90cf07f6100baa4cd808
Robert Martins <robert.rm131@gmail.com>
2021-09-29 13:41:44 -0300

    ajustar lint para requisito 4 e correr para o abraço

--- test/setupTeardown.spec.js
+++ test/setupTeardown.spec.js
@@ -23,7 +23,7 @@ describe('4 - Quem sobreviveu?', () => {
   // Adicione seu código aqui
   beforeEach(() => {
     adventure.randomAttack();
-  })
+  });
 
   test('depois da primeira aventura', () => {
     expect(adventure.specialists.length).toBe(5);
